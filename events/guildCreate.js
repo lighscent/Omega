7c0451ca17d5710e155c0879d5d243f31423c3c9
Азукиов <93555197+Azukiov@users.noreply.github.com>
2023-11-19 14:46:02 +0100

    remake the bot for only one server

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -8,18 +8,6 @@ module.exports = {
     async execute(guild) {
         try {
             const guild_Id = guild.id;
-
-            db.query(`INSERT IGNORE INTO guilds (guildId) VALUES ('${guild_Id}')`);
-            log.db(`Added guild ${guild_Id} to table guilds`);
-            db.query(`INSERT IGNORE INTO modules (guildId) VALUES ('${guild_Id}')`);
-            log.db(`Added guild ${guild_Id} to table modules`);
-
-            const users = await guild.members.fetch();
-            users.forEach(user => {
-                const userId = user.id;
-                db.query(`INSERT IGNORE INTO feurByUser (userId) VALUES ('${userId}')`);
-                log.db(`Added user ${userId} to table feurByUser`);
-            });
             
         } catch (error) {
             log.error(error);
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -10,22 +10,7 @@ module.exports = {
 
     async execute(message) {
         try {
-            const feurModule = await db.query(`SELECT feurModule FROM modules WHERE guildId = '${message.guild.id}'`);
-
-
             if (message.author.bot) return;
-
-            //check in db if module feur is true
-            if (feurModule[0].feurModule === true) {
-                if (message.content.toLowerCase() === "quoi") {
-                    db.query(`UPDATE feurByUser SET feurCount = feurCount + 1 WHERE userId = '${message.author.id}'`);
-                    db.query(`UPDATE feurByGuild SET feurCount = feurCount + 1 WHERE guildId = '${message.guild.id}'`);
-                    db.query(`UPDATE botStats SET feurCount = feurCount + 1`);
-                    const feurCount = await db.query(`SELECT feurCount FROM botStats`);
-                    message.channel.send("feur #" + feurCount[0].feurCount);
-                }    
-            } else return log.debug(`Module feur is not enabled in guild ${message.guild.id}`)
-
         } catch (error) {
             log.error(error);
         }
