c160931ab07274104e4f5d460bc62677da575a7c
Азукиов <93555197+Azukiov@users.noreply.github.com>
2023-11-20 09:39:11 +0100

    v1.2.1

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,16 +7,17 @@ module.exports = {
 
     async execute(member) {
         try {
-            const channel = member.guild.channels.cache.find(ch => ch.id === '1157676909962985493');
+            const channel = member.guild.channels.cache.find(ch => ch.id === '1141804157599228014');
             if (!channel) return;
 
             const guildCount = member.guild.memberCount;
 
             const embedJoin = new djs.EmbedBuilder()
-                .setTitle(`[+] Welcome ${member.user.username}!`)
-                .setDescription(`id:\`${member.user.id}\``)
-                .setFooter({ text: `${guildCount} members`})
-                .setTimestamp();
+                .setDescription(`user: \`${member.user.tag}\` \n id: \`${member.user.id}\``)
+                .setFooter({ text: `${guildCount} members` })
+                .setTimestamp()
+                .setColor('Green');
+                
 
             await channel.send({ embeds: [embedJoin] });
         } catch (error) {
